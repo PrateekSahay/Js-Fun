7bea6641dfa732222eae78014d0f360ed6444d05
Prateek <prateeksahay96@gmail.com>
2024-02-05 02:02:08 +0530

    Added deepcopy

---
+++ deepCopy.js
@@ -0,0 +1,20 @@
+const obj = {
+    a1: {
+        b1: {
+            c1: "SS"
+        }
+     }
+ }
+ const deepCopy = (val) => {
+    if (["string", "boolean", "number"].includes(typeof val)) {
+       return val;
+    } else if (Array.isArray(val)) {
+       return val.map(item => deepCopy(item))
+    } else {
+       return Object.keys(val).reduce((acc, key) => {
+         acc[key] = deepCopy(val[key]);
+         return acc;
+      }, {});
+   }
+ }
+ console.log(deepCopy(obj));
