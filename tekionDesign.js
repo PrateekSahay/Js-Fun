84e8d2da16bb4ca62e54b683caaf520d3d3d157a
Prateek <prateeksahay96@gmail.com>
2024-03-06 23:19:44 +0530

    Added tekion design ques

---
+++ tekionDesign.js
@@ -0,0 +1,44 @@
+// repo -> multiple projects -> ecommerce / pages (home/productList/productDetails/checkout)
+// more applications -> admin/cms-portall
+
+//monoRepo/MicroFE etc.
+
+// multiple teams independently work
+
+// BE -> contracts / React / Vue / Angular
+// src -> HomeRouter -> home/productList/productDetails/checkout)
+
+// src/ Routes
+// src/component -> react files/css/test cases
+// src/GenericComponents
+// eslint
+// tsconfig
+// webpack -> dev/prod
+
+// axios -> get/post/patch/delete
+
+// axios.create({baseApi}) -> axios1
+// auth token -> baseApi -> /api
+
+// intercept each api / gatekeeping / allow if certain validation ->
+
+// wrapper ->
+
+// class Axios -> constructor() / get / post /create
+// class props -> baseApi
+// this.metaData
+
+// new Axios({get/post/create/etx})
+
+// create (metaData) {
+// this.metaData = metaData;
+// new Axios(metaData)
+// }
+
+//axios.get("url")
+
+// get ('url', 'baseUrl') {
+// if // baseUrl -> propery ? property
+// }
+
+//
